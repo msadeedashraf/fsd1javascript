251e3ad11c1505f204b0d9fbaeaaefb9f2f9603e
Sadeed <msadeedashraf@gmail.com>
2024-04-29 11:36:07 -0400

    move-app with promise done

---
+++ projects/Movie-App/examples.js
@@ -0,0 +1,15 @@
+/*
+
+function name_of_the_function(num1, num2) {
+  return num1 + num2;
+}
+
+//name_of_the_function(5,5);
+var result = name_of_the_function(5, 5);
+console.log(result);
+*/
+
+let name_of_the_function = (num1 = 5, num2 = 9) => num1 + num2;
+console.log(name_of_the_function());
+
+//const nvbmnbcxmnbvnmcxbv = () => {};
